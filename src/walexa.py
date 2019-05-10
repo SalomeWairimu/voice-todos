7bb94a4e926ac4a91ca74edab633fb066bfab780
ulyanakurylo <ulyanakurylo2020@u.northwestern.edu>
2019-05-10 12:05:58 -0500

    revamped nlp + made classes

--- src/walexa.py
+++ src/walexa.py
@@ -3,44 +3,65 @@ import sys
 import math
 import struct
 import time
+import csv
 
 
-def similar(a,b):
-	return SequenceMatcher(None,a,b).ratio()
+class obj:
+	def __init__(self, name):
+		self.name = name
+		self.who = None
+		self.where = None 
+		self.what = None 
+		self.why = None 
+		self.when = None 
+		self.whispers = None 
 
+	def get_ans(self, qst):
+		qst = qst.lower()
+		ans = ""
+		if "who" in qst:
+			ans = self.who
+		elif "where" in qst:
+			ans = self.where
+		elif "what" in qst:
+			ans = self.what
+		elif "why" in qst:
+			ans = self.why
+		elif "when" in qst:
+			ans= self.when
+		else:
+			ans = self.whispers
+		return ans
 
-
-question_database={}
-
-qst_audioID = {}
-
-def populate_Id_dict():
-	qst_audioID["biconical bead"] = ["1.mp3"]
-
-def populate_qst_database():
-	data=open("data.csv","r")
-	lines=data.readlines()
-	for line in lines:
-		q_and_a=line.split(",")
-		question_database[q_and_a[0]]=q_and_a[1]
+objects = []
+def initialize_everything():
+	catobj = obj(name='the cat')
+	catobj.who='Hyun Jung Jun  an art student at Northwestern University'
+	catobj.what = "The cat is a flashe  linen  thread piece. Flashe is a vinyl-based  which can be used to create paintings on various mediums  such as linen and thread."
+	catobj.where = 'The cat was created in Evanston  IL'
+	catobj.why = 'For a thesis project'
+	catobj.when = 'This was created in 2019'
+	cruxobj = obj(name = 'the crucifixion')
+	cruxobj.who= 'Naddo Ceccarelli'
+	cruxobj.what = 'The crucifixion is a Tempera and gold on panel  gold leaf is gilded onto the wood panel before inscription and detailing'
+	cruxobj.where = 'It was created in Siena Ital'
+	cruxobj.why = 'The crucfixion is a depiction of Christs death  used in many Churches. These panel works were typically used in reliquaries'
+	cruxobj.when = 'The crucifixion was created in the 14th century  circa 1350-1359'
+	objects.append(catobj)
+	objects.append(cruxobj)
+		
 
 
 def create_answer():
 	question=open("question.txt","r").read()
-	best_ratio=0
-	best_key="N/A"
-	for k in question_database.keys():
-		if (similar(question,str(k))>best_ratio):
-			best_ratio=similar(question,str(k))
-			best_key=k
-	if "Custom" in question_database[best_key].strip():
-		answ = qst_audioID[best_key]
-	else:
-		answ = question_database[best_key]
+	answ = "I don't understand"
+	for elem in objects:
+		if elem.name in question:
+			print(elem.name)
+			answ = elem.get_ans(question)
 	f=open("answer.txt","w")
 	f.write(str(answ))
 
 
-populate_Id_dict()
-populate_qst_database()
+initialize_everything()
 create_answer()
